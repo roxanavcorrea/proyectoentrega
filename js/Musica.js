219fd55deac0ac6de90770054e60b167d32bff41
roxana correa <roxanacorrea33@gmail.com>
2024-05-05 22:14:45 -0300

    entrega final

--- js/Musica.js
+++ js/Musica.js
@@ -1,77 +1,51 @@
-let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("El DOM ha sido cargado correctamente.");
+    
+    let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+    const contenedorProductos = document.querySelector("#albumcompleto");
+    const carritoVacio = document.querySelector("#carrito-vacio");
+    const carritoProductos = document.querySelector("#carrito-productos");
+    const carritoTotal = document.querySelector("#carrito-total");
 
-const productos = [
-    {
-        titulo: "My World",
-        precio: 6500,
-        img: "../img/1200x1200bf-60.jpg",
-    },
-    {
-        titulo: "Journals",
-        precio: 8500,
-        img: "../img/6691017_orig.png",
-    },
-    {
-        titulo: "My World Acoustic",
-        precio: 8000,
-        img: "../img/justin bieber my world unplugged.jpg",
-    },
-    {
-        titulo: "Purpose",
-        precio: 10000,
-        img: "../img/justin_bieber_purpose_vinyl_front_cover_f3c094ef-dd75-4351-89c0-2ac74f8e64ea_1024x1024.webp",
-    },
-    {
-        titulo: "The Mistletoe",
-        precio: 9000,
-        img: "../img/under-the-mistletoe-photo-1.jpg",
-    },
-    {
-        titulo: "Believe",
-        precio: 8000,
-        img: "../img/justinb_believe_coverar_3000dpi300rgb1000181196_1.jpg",
-    }
+    fetch("/data/productos.json") 
+        .then(response => response.json())
+        .then(data => {
+            console.log("Datos obtenidos del archivo JSON:", data);
+        
+            data.forEach(producto => {
+                const div = document.createElement("div");
+                div.innerHTML = `
+                    <img class="albumcompleto" src="${producto.img}" alt="${producto.titulo}">
+                    <h3>${producto.titulo}</h3>
+                    <p>$${producto.precio}</p>
+                `;
 
-];
+                const btn = document.createElement("button");
+                btn.classList.add("boton");
+                btn.innerText = "Agregar Al Carrito";
 
-const contenedorProductos = document.querySelector("#albumcompleto");
-const carritoVacio = document.querySelector("#carrito-vacio");
-const carritoProductos = document.querySelector("#carrito-productos");
-const carritoTotal = document.querySelector("#carrito-total");
+                btn.addEventListener("click", () => {
+                    agregarAlCarrito(producto);
+                })
 
-productos.forEach((producto) => {
-    const div = document.createElement("div");
-    div.innerHTML = `
-        <img class="albumcompleto" src="${producto.img}" alt="${producto.titulo}">
-        <h3>${producto.titulo}</h3>
-        <p>$${producto.precio}<//p>
-    `;
+                div.append(btn);
+                contenedorProductos.append(div);
+            });
+        });
 
-    const btn = document.createElement("button");
-    btn.classList.add("boton");
-    btn.innerText = "Agregar Al Carrito";
+    function actualizarCarrito() {
+        if (carrito.length === 0) {
+            carritoVacio.classList.remove("d-none");
+            carritoProductos.classList.add("d-none");
+        } else {
+            carritoVacio.classList.add("d-none");
+            carritoProductos.classList.remove("d-none");
 
-    btn.addEventListener("click", () => {
-        agregarAlCarrito(producto);
-    })
-
-    div.append(btn);
-    contenedorProductos.append(div);
-})
-
-function actualizarCarrito() {  
-    if (carrito.length === 0) {
-        carritoVacio.classList.remove("d-none");
-        carritoProductos.classList.add("d-none");
-    } else {
-        carritoVacio.classList.add("d-none");
-        carritoProductos.classList.remove("d-none");
-
-        carritoProductos.innerHTML = "";
-        carrito.forEach(producto => {
-            const div = document.createElement("div");
-            div.classList.add("carrito-producto");
-            div.innerHTML = `
+            carritoProductos.innerHTML = "";
+            carrito.forEach(producto => {
+                const div = document.createElement("div");
+                div.classList.add("carrito-producto");
+                div.innerHTML = `
             <img class="albumcompleto" src="${producto.img}">
             <h3>${producto.titulo}</h3>
             <p>$${producto.precio}</p>
@@ -79,39 +53,43 @@ function actualizarCarrito() {
             <p>Subt: $${producto.cantidad * producto.precio}</p>
             `;
 
-            const btn = document.createElement("button");
-            btn.classList.add("carrito-producto-btn");
-            btn.innerText = "❌​​"
-            btn.addEventListener("click", () => {
-                borrarDelCarrito(producto);
+                const btn = document.createElement("button");
+                btn.classList.add("carrito-producto-btn");
+                btn.innerText = "❌​​"
+                btn.addEventListener("click", () => {
+                    borrarDelCarrito(producto);
+                })
+                div.append(btn);
+
+                carritoProductos.append(div);
             })
-            div.append(btn);
+        }
+        actualizarTotal();
 
-            carritoProductos.append(div);
-        })
-    }
-    actualizarTotal();
-
-}
-
-const agregarAlCarrito = (producto) => {
-    const itemEncontrado = carrito.find(item => item.titulo === producto.titulo);
-    if (itemEncontrado) {
-        itemEncontrado.cantidad++;
-    } else {
-        carrito.push({...producto, cantidad: 1 });
     }
 
-    actualizarCarrito();
-}
+    const agregarAlCarrito = (producto) => {
+        console.log("Agregando producto al carrito:", producto);
+        const itemEncontrado = carrito.find(item => item.titulo === producto.titulo);
+        if (itemEncontrado) {
+            itemEncontrado.cantidad++;
+        } else {
+            carrito.push({ ...producto, cantidad: 1 });
+        }
 
-const borrarDelCarrito = (producto) => {
-    const prodIndex = carrito.findIndex(item => item.titulo === producto.titulo);
-    carrito.splice(prodIndex, 1);
-    actualizarCarrito();
-}
+        actualizarCarrito();
+    }
 
-const actualizarTotal = () => {
-    const total = carrito.reduce((acc, prod) => acc + (prod.precio * prod.cantidad), 0);
-    carritoTotal.innerText = `$${total}`;
-}
+    const borrarDelCarrito = (producto) => {
+        console.log("Borrando producto del carrito:", producto);
+        const prodIndex = carrito.findIndex(item => item.titulo === producto.titulo);
+        carrito.splice(prodIndex, 1);
+        actualizarCarrito();
+    }
+
+    const actualizarTotal = () => {
+        console.log("Actualizando total del carrito...");
+        const total = carrito.reduce((acc, prod) => acc + (prod.precio * prod.cantidad), 0);
+        carritoTotal.innerText = `$${total}`;
+    }
+});
--- js/main.js
+++ js/main.js
@@ -2,24 +2,30 @@ function toggleMode() {
     const body = document.body;
     const boton = document.querySelector('.boton');
     body.classList.toggle('dark-mode');
-    boton.classList.toggle('boton-light'); // Cambia la clase del botón al modo claro
-    console.log(modo); 
+    boton.classList.toggle('boton-light'); 
+    console.log(modo);
 }
 
 document.addEventListener("DOMContentLoaded", function () {
+    console.log("El DOM ha sido cargado correctamente.");
     // Función para validar la edad (convertida a función de flecha)
     const validarEdad = (edad) => {
+        console.log("Validando edad:", edad);
         if (edad === null || isNaN(edad) || edad === "") {
-            return false; // La edad no es válida
+            console.log("La edad no es válida.");
+            return false; 
         } else {
             edad = parseInt(edad);
+            console.log("Edad válida:", edad);
             return edad >= 18; // La persona es mayor de edad
         }
     };
 
     // Función para manejar la suscripción del usuario
     const manejarSuscripcion = () => {
+        console.log("Manejando suscripción del usuario.");
         let edad = prompt("Ingrese su edad:");
+        console.log("Edad ingresada por el usuario:", edad);
         if (validarEdad(edad)) {
             // Almacenar datos del usuario en localStorage utilizando JSON
             const usuario = {
@@ -31,12 +37,15 @@ document.addEventListener("DOMContentLoaded", function () {
             // Almacenar indicador de suscripción
             localStorage.setItem('usuario_suscrito', true);
 
-            alert("¡Gracias por suscribirte!");
+            alert ("¡Gracias por suscribirte!");
             // Redirigir a la página de registro si es mayor de 18 años
             window.location.href = "./html/Registrate.html";
         } else {
-            alert("Lo siento, necesitas ser mayor de 18 años para suscribirte.");
-
+            Swal.fire ({ 
+                title: "Lo siento, necesitas ser mayor de 18 años para suscribirte.",
+                text: "¡Oops!",
+                icon: "error"
+                });
             // Ocultar la opción de suscripción si es menor de 18 años
             const subscribeMessage = document.getElementById('subscribe-message');
             subscribeMessage.style.display = 'none';
@@ -47,15 +56,16 @@ document.addEventListener("DOMContentLoaded", function () {
     const subscribeMessage = document.getElementById('subscribe-message');
     subscribeMessage.addEventListener('click', manejarSuscripcion);
 
-    // Función para manejar la selección de opciones del usuario (convertida a función de flecha)
+    
     const manejarOpciones = () => {
+        console.log("Manejando opciones del usuario.");
         // Usuario
         let usuario = prompt("Bienvenido! Ingrese su nombre:");
         while (usuario === "" || usuario === null) {
             usuario = prompt("Debes ingresar un nombre válido para continuar. Por favor, ingrese su nombre:");
         }
         console.log("Usuario:", usuario);
-        alert("Bienvenido, " + usuario + ", a la fan page de tu cantante favorito!");
+        alert ("Bienvenido, " + usuario + ", a la fan page de tu cantante favorito!");
 
         // Edad
         let edad = prompt("Ingrese su edad:");
@@ -63,13 +73,17 @@ document.addEventListener("DOMContentLoaded", function () {
 
         // Validar la edad utilizando la función validarEdad
         if (!validarEdad(edad)) {
-            alert("Lo siento, necesitas ser mayor de 18 años para ingresar a esta página.");
+            Swal.fire ({ 
+                title: "Lo siento, necesitas ser mayor de 18 años para ingresar a esta página.",
+                text: "¡Error!",
+                icon: "error"
+                });
             // Detener la ejecución del script si la edad no es válida
             throw new Error("Edad no válida");
         } else if (parseInt(edad) === 18) {
-            alert("¡Tenes 18 años! Bienvenido a la fan page.");
+            alert ("¡Tenes 18 años! Bienvenido a la fan page.");
         } else {
-            alert("Bienvenido a la fan page.");
+            alert ("Bienvenido a la fan page.");
         }
 
         // Array para almacenar las selecciones de opciones del usuario
@@ -91,44 +105,44 @@ document.addEventListener("DOMContentLoaded", function () {
 
                 switch (opcion) {
                     case 0:
-                        alert("¡Gracias por visitarnos!");
+                        Swal.fire ("¡Gracias por visitarnos!");
                         break;
                     case 1:
-                        alert("¡Elegiste Música! ¡Que lo disfrutes!");
+                        alert ("¡Elegiste Música! ¡Que lo disfrutes!");
                         window.location.href = "./html/Musica.html";
                         break;
                     case 2:
-                        alert("¡Elegiste Videos! ¡Que lo disfrutes!");
+                        alert ("¡Elegiste Videos! ¡Que lo disfrutes!");
                         window.location.href = "./html/Video.html";
                         break;
                     case 3:
-                        alert("¡Elegiste Tour! ¡Que lo disfrutes!");
+                        alert ("¡Elegiste Tour! ¡Que lo disfrutes!");
                         window.location.href = "./html/Tour.html";
                         break;
                     case 4:
-                        alert("¡Elegiste Registrarse! Gracias por unirte.");
+                        alert ("¡Elegiste Registrarse! Gracias por unirte.");
                         window.location.href = "./html/Registrate.html";
                         break;
                 }
             } else {
-                alert("Opción no válida. Por favor, selecciona una opción del 0 al 4.");
+                alert ("Opción no válida. Por favor, selecciona una opción del 0 al 4.");
             }
         } while (isNaN(opcion) || opcion < 0 || opcion > 4);
 
         // Filtrar las selecciones del usuario para obtener solo las opciones mayores a 2
         const seleccionesFiltradas = seleccionesUsuario.filter(opcion => opcion > 2);
-        let mensaje = ""; // Variable para almacenar el mensaje
+        let mensaje = ""; 
 
         for (let i = 0; i < seleccionesFiltradas.length; i++) {
             mensaje += `El usuario seleccionó la opción ${seleccionesFiltradas[i]}.\n`;
         }
 
         if (mensaje !== "") {
-            console.log(mensaje); // Imprimir el mensaje en la consola
+            console.log(mensaje); 
         } else {
             console.log("El usuario no seleccionó opciones mayores a 2.");
         }
     };
 
-    manejarOpciones(); // Llamada inicial a manejarOpciones
+    manejarOpciones(); 
 });
